4ab24332519d0206bd7c6cb444db4d01a4f75705
= <fivoneto@outlook.com>
2015-01-03 00:27:26 +0200

    Selector tests added

--- test/test.js
+++ test/test.js
@@ -3,52 +3,86 @@ var jsdom = require("jsdom").jsdom;
 var fs = require("fs");
 var assert = require("assert");
 
-var toCssPath = require("../lib/to_css_path.js");
-var toJSON = require("../lib/util/to_json.js");
+// var toCssPath = require("../lib/to_css_path.js");
+// var toJSON = require("../lib/util/to_json.js");
 
-var doc = jsdom(fs.readFileSync(__dirname + "/html_env/test_env.html")),
-		window = doc.parentWindow,
-		document = window.document;
+// var doc = jsdom(fs.readFileSync(__dirname + "/html_env/test_env.html")),
+// 		window = doc.parentWindow,
+// 		document = window.document;
 
-toCssPath.install(window.HTMLElement, document, true);
-toJSON.install(window.HTMLElement);
+// toCssPath.install(window.HTMLElement, document, true);
+// toJSON.install(window.HTMLElement);
 
-function elNametoCssPath(name){
-	var el = document.querySelector("[data-el-name='"+ name +"']");
-	if(!el){
-		return undefined;
-	}
-	if(!el.toCssPath){
-		throw new Error("elNameToElement: toCssPath not installed on given element");
-	}
+describe("Selectors", function(){
+	var Selector = require("../lib/selectors/selector.js");
+	var IdSelector = require("../lib/selectors/id_selector.js");
+	var ClassSelector = require("../lib/selectors/class_selector.js");
+	var TagSelector = require("../lib/selectors/tag_selector.js");
+	var NthSelector = require("../lib/selectors/nth_selector.js");
+	var ParentSelector = require("../lib/selectors/parent_selector.js");
 
-	return el.toCssPath();
-}
+	describe("All of them", function(){
+		it("should initalize with correct type and value", function(){
+			var idS = new IdSelector("a");
+			assert.deepEqual(Selector.TYPE_ID, idS.type);
+			assert.deepEqual(idS.value, "a");
 
-function query(q){
-	return document.querySelector(q);
-}
+			var classS = new ClassSelector("b");
+			assert.deepEqual(Selector.TYPE_CLASS, classS.type);
+			assert.deepEqual(classS.value, "b");
 
-describe("toCssPath", function(){
-	var names = "abcdejklmn".split("");
+			var tagS = new TagSelector("c");
+			assert.deepEqual(Selector.TYPE_TAG, tagS.type);
+			assert.deepEqual(tagS.value, "c");
 
-	it("should give right css path", function(){
-		assert.deepEqual(elNametoCssPath("a"), "fancyspan");
-		assert.deepEqual(elNametoCssPath("b"), "span#foo");
-		assert.deepEqual(elNametoCssPath("c"), "span.fee.faa");
-		assert.deepEqual(elNametoCssPath("d"), "span.fee.foo:nth-child(1)");
-		assert.deepEqual(elNametoCssPath("e"), "span.fit.fat:nth-child(1)");
-		assert.deepEqual(elNametoCssPath("j"), "div.foo > div:nth-child(1) > span:nth-child(1)");
-		assert.deepEqual(elNametoCssPath("k"), "p:nth-child(2)");
-		assert.deepEqual(elNametoCssPath("l"), "ul:nth-child(1) > li:nth-child(3)");
-		assert.deepEqual(elNametoCssPath("m"), "script:nth-child(14)");
-		assert.deepEqual(elNametoCssPath("n"), "span#nee\\.fee");
+			var nthS = new NthSelector("d");
+			assert.deepEqual(Selector.TYPE_NTH, nthS.type);
+			assert.deepEqual(nthS.value, "d");
+		});
+
+		it("should give correct string when 'toString' method called", function(){
+			var idS = new IdSelector("aa");
+			assert.deepEqual(idS.toString(), "#aa");
+
+			// TODO: ClassSelector
+
+			var tagS = new TagSelector("cc");
+			assert.deepEqual(tagS.toString(), "cc");
+
+			var nthS = new NthSelector("dd");
+			assert.deepEqual(nthS.toString(), ":nth-child(dd)");
+		});
 	});
 
-	it("should give valid css path to correct dom element", function(){
-		for(var i = 0; i < names.length; i++){
-			var n = names[i];
-			assert.deepEqual(document.querySelector(elNametoCssPath(n)), document.querySelector("[data-el-name='"+ n +"']"));
-		}
-	})
+	describe("Selector", function(){
+		describe("#constructor()", function(){
+			it("should initialize the own properties by given paramaters", function(){
+				var s = new Selector(0, "a");
+
+				assert.deepEqual(s.type, 0);
+				assert.deepEqual(s.value, "a");
+			});
+
+			it("should give every instance a unique id", function(){
+				var selectorIdPool = [];
+
+				for(var i = 0; i < 10; i++){
+					var s = new Selector(i, new String(i * 2));
+					
+					assert.ok(selectorIdPool.indexOf(s.id) === -1);
+
+					selectorIdPool.push(s.id);
+				}
+			});
+		});
+
+		describe("#toString()", function(){
+			it("should throw a error if its not implemented/overrided", function(){
+				var s = new Selector();
+				assert.throws(function(){
+					s.toString();
+				});
+			});
+		});
+	});
 });
